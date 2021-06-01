ad89118e89eea44f089320df0a5a7152b44438e9
YellowFlash2012 <cyclop9@hotmail.com>
2021-06-01 09:32:11 +0100

    Add index.html style.css app.js

--- Exchange Rate Calculator/app.js
+++ Exchange Rate Calculator/app.js
@@ -17,19 +17,16 @@ function calculate() {
     const currency_two = currencyEl_two.value;
     
 
-    fetch('https://v6.exchangerate-api.com/v6/f33739e35747f899ec8ae9b0/latest/USD')
-        .then(res => res.json())
-        .then(data => {
+    fetch(`https://v6.exchangerate-api.com/v6/f33739e35747f899ec8ae9b0/latest/${currency_one}`).then(res => res.json()).then(data => {
             
-            const rate = data.rates(currency_two) / data.rates(currency_one);
+            const rate = data.rates[currency_two];
             
             rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
 
             amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
         });
-
-    
 }
+
 
 //Events listeners
 currencyEl_one.addEventListener('change', calculate);
